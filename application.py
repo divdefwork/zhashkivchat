5b892e2fa44a03bcdaf953d3c59ac56808d61564
divdefwork <divdefwork@gmail.com>
2020-09-08 21:09:15 +0300

    Chat App Part 12 all

--- application.py
+++ application.py
@@ -15,11 +15,12 @@ app.secret_key = 'replace later'
 
 # Налаштування бази даних
 app.config['SQLALCHEMY_DATABASE_URI'] = 'postgres://ywqxdfutjbrllu:4bf9490496b00c99e12a00ac7aad76018a943dd7997fb6667da8dc33b63dd075@ec2-54-228-209-117.eu-west-1.compute.amazonaws.com:5432/dc4d1f73beffi3'
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 db = SQLAlchemy(app)
 
 # Ініціалізуйте Flask-SocketIO
-socketio = SocketIO(app)
-ROOMS = ["вітальня", "продам/куплю", "реклама", "питання/відповідь"]
+# socketio = SocketIO(app)
+# ROOMS = ["вітальня", "продам/куплю", "реклама", "питання/відповідь"]
 
 # Налаштування flask login
 login = LoginManager(app)
@@ -30,6 +31,12 @@ login.init_app(app)
 def load_user(id):
 
     return User.query.get(int(id))
+
+
+socketio = SocketIO(app, manage_session=False)
+
+# Наперед визначені кімнати для чату
+ROOMS = ["вітальня", "продам/куплю", "реклама", "питання/відповідь"]
 
 
 @app.route('/', methods=['GET', 'POST'])
@@ -91,15 +98,40 @@ def logout():
     return redirect(url_for('index'))
 
 
-@socketio.on('message')
-def message(data):
+@socketio.on('incoming-msg')
+def on_message(data):
+    """Трансляція повідомлень"""
 
-    print(f"\n\n{ data }\n\n")
+    msg = data["msg"]
+    username = data["username"]
+    room = data["room"]
+    # Встановити позначку часу
+    time_stamp = time.strftime('%H:%M %d %B', time.localtime())
+    send({"username": username, "msg": msg, "time_stamp": time_stamp}, room=room)
 
-    send({'msg': data['msg'], 'username': data['username'],
-          'time_stamp': strftime('%H:%M %d %B', localtime())},
-         room=data['room'])
+
+@socketio.on('join')
+def on_join(data):
+    """Користувач приєднується до кімнати"""
+
+    username = data["username"]
+    room = data["room"]
+    join_room(room)
+
+    # Трансляція, до якої приєднався новий користувач
+    send({"msg": username + " приєднався до кімнати" + room + "."}, room=room)
+
+
+@socketio.on('leave')
+def on_leave(data):
+    """Користувач залишає кімнату"""
+
+    username = data['username']
+    room = data['room']
+    leave_room(room)
+    send({"msg": username + " вийшов із кімнати"}, room=room)
 
 
 if __name__ == "__main__":
-    socketio.run(app, debud=True)
+    # socketio.run(app, debud=True)
+    app.run(debug=True)
